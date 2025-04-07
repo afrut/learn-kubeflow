cec49f42f64f4cccd9a98411fa9f4d98ecf83cd3
afrut19 <25942990+eric373@users.noreply.github.com>
2025-04-06 21:45:10 -0400

    Basic pipeline

--- kubeflow_pipelines/basic.py
+++ kubeflow_pipelines/basic.py
@@ -3,38 +3,32 @@ import os
 KFP_ENDPOINT = os.environ["KFP_ENDPOINT"]
 
 
-# from kfp import dsl
+from kfp import dsl
 
 
-# @dsl.component
-# def say_hello(name: str) -> str:
-#     hello_text = f"Hello, {name}!"
-#     print(hello_text)
-#     return hello_text
+@dsl.component
+def say_hello(name: str) -> str:
+    hello_text = f"Hello, {name}!"
+    print(hello_text)
+    return hello_text
 
 
-# @dsl.pipeline
-# def hello_pipeline(recipient: str) -> str:
-#     hello_task = say_hello(name=recipient)
-#     return hello_task.output
+@dsl.pipeline
+def hello_pipeline(recipient: str) -> str:
+    hello_task = say_hello(name=recipient)
+    return hello_task.output
 
 
-# from kfp import compiler
+from kfp import compiler
 
-# compiler.Compiler().compile(hello_pipeline, "pipeline.yaml")
+compiler.Compiler().compile(hello_pipeline, "pipeline.yaml")
 
-# from kfp.client import Client
+from kfp.client import Client
 
-# client = Client(host=KFP_ENDPOINT)
-# run = client.create_run_from_pipeline_package(
-#     "pipeline.yaml",
-#     arguments={
-#         "recipient": "World",
-#     },
-# )
-
-import kfp
-
-client = kfp.Client(host=KFP_ENDPOINT)
-
-print(client.list_experiments())
+client = Client(host=KFP_ENDPOINT)
+run = client.create_run_from_pipeline_package(
+    "pipeline.yaml",
+    arguments={
+        "recipient": "World",
+    },
+)
